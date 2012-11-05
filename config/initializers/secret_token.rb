f2fb95dfe59a415f22bf08a21c89e83df2367925
Ramachandran <ramachandran@bootrock.com>
2012-11-05 20:19:35 +0530

    Initial commit - Created the initial app with basic configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineAirlinePortal::Application.config.secret_token = '9e742f3f0b6b455b3d159b3075f77b55082fb7b24aa3147fc51047938dfb0664f75efab7070e4bd185a23e33e9c54bf6a22f0f193e561290b4b4b1194c25a1e3'
