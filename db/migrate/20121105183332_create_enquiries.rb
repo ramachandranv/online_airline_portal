36f14416c2b6bee6a7c897a6b28fea60da53694f
Ramachandran <ramachandran@bootrock.com>
2012-11-10 15:45:29 +0530

    Change in migration, added title for all views and change in mail format

--- db/migrate/20121105183332_create_enquiries.rb
+++ db/migrate/20121105183332_create_enquiries.rb
@@ -1,19 +1,19 @@
 class CreateEnquiries < ActiveRecord::Migration
   def change
     create_table :enquiries do |t|
-      t.string :trip_type
-      t.string :source
-      t.string :destination
-      t.string :departure_date
-      t.string :return_date
-      t.string :adults
-      t.string :children
-      t.string :infants
-      t.string :class_of_travel
-      t.string :airline_preference
-      t.string :departure_time
-      t.string :return_time
-      t.text   :passenger_details
+      t.string  :trip_type
+      t.string  :source
+      t.string  :destination
+      t.date    :departure_date
+      t.date    :return_date
+      t.integer :adults
+      t.integer :children
+      t.integer :infants
+      t.string  :class_of_travel
+      t.string  :airline_preference
+      t.string  :departure_time
+      t.string  :return_time
+      t.text    :passenger_details
       t.references :user
 
       t.timestamps
